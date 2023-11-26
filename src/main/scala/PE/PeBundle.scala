23715f8b11f56bd01f364bfd0b105e80962cbe01
byao <eebyao@ust.hk>
2023-11-26 09:49:14 +0800

    pe connect done

--- src/main/scala/PE/PeBundle.scala
+++ src/main/scala/PE/PeBundle.scala
@@ -9,14 +9,7 @@ import spinal.lib.fsm._
 case class PeBundle(config: PeBundleConfig) extends Component {
 
     val io_fifo       =  new Bundle {
-        val pe1_fifo = slave Stream (Bits(config.axi_width bits))
-        val pe2_fifo = slave Stream (Bits(config.axi_width bits))
-        val pe3_fifo = slave Stream (Bits(config.axi_width bits))
-        val pe4_fifo = slave Stream (Bits(config.axi_width bits))
-        val pe5_fifo = slave Stream (Bits(config.axi_width bits))
-        val pe6_fifo = slave Stream (Bits(config.axi_width bits))
-        val pe7_fifo = slave Stream (Bits(config.axi_width bits))
-        val pe8_fifo = slave Stream (Bits(config.axi_width bits))
+        val pe_fifo = vec(slave Stream (Bits(config.axi_width bits)), 8)
     }
 
     val io_global_reg =new Bundle {
@@ -30,8 +23,13 @@ case class PeBundle(config: PeBundleConfig) extends Component {
     }
 
     val io_update_reg = new Bundle {
-        val pe1_update_reg_addr = out Bool()
-        val switch_done = in Bool()
+        val update_reg_wr_valid = vec(out Bool(), 8)
+        val update_reg_wr_addr = vec(out UInt (config.update_ram_addr_width bits), 8)
+        val update_reg_wr_data = vec(out Bits (config.vertex_width bits), 8)
+
+        val update_ram_rd_addr = vec(out UInt (config.update_ram_addr_width bits), 8)
+        val update_ram_rd_data = vec(in Bits (config.vertex_width bits))
+    
     }
 
     val global_reg          =   GlobalReg (config.global_reg_config)
@@ -40,6 +38,7 @@ case class PeBundle(config: PeBundleConfig) extends Component {
 
 //  1 pe bundle contain 8 pe threads
     val pe_bundle  = new Array[PeCore](8)
+
     for (i <- 0 until 8) {
         pe_bundle (i) = PeCore(config.pe_core_config)
         pe_bundle (i).setName("pe_" + i.toString)
@@ -48,38 +47,26 @@ case class PeBundle(config: PeBundleConfig) extends Component {
         pe_bundle(i).io_state.switch_done <> io_state.switch_done
         pe_bundle(i).io_state.globalreg_done <> io_state.switch_done
         global_reg.io.reg_full <> pe_bundle(i).io_state.globalreg_done
-// TO DO:
-// global reg need 8 pe rd address and data
-        global_reg.io.rd_addr <> pe_bundle(i).io_vertex_reg.vertex_reg_addr
-        global_reg.io.rd_data <> pe_bundle(i).io_vertex_reg.vertex_reg_addr
 
-// TO DO:
-// Uupdate reg need 8 pe rd address and data
+        pe_bundle(i).io_vertex_reg.vertex_reg_addr <> global_reg.io.rd_addr(i)
+        pe_bundle(i).io_vertex_reg.vertex_reg_addr <> global_reg.io.rd_data(i)
 
-//        val io_update_ram = new Bundle {
-//            val update_ram_wr_valid = out Bool()
-//            val update_ram_wr_addr = out Bits (config.update_ram_addr_width bits)
-//            val update_ram_wr_data = out Bits (32 bits)
-//
-//            val update_ram_rd_valid = out Bool()
-//            val update_ram_rd_addr = out Bits (config.update_ram_addr_width bits)
-//            val update_ram_rd_data = in Bits (32 bits)
+        pe_bundle(i).io_update_ram.update_ram_wr_valid <>  io_update_reg.update_reg_wr_valid(i)
+        pe_bundle(i).io_update_ram.update_ram_wr_addr <>  io_update_reg.update_reg_wr_addr(i)
+        pe_bundle(i).io_update_ram.update_ram_wr_data <>  io_update_reg.update_reg_wr_data(i)
+
+        pe_bundle(i).io_update_ram.update_ram_rd_addr <>  io_update_reg.update_ram_rd_addr(i)
+        pe_bundle(i).io_update_ram.update_ram_rd_data <>  io_update_reg.update_ram_rd_data(i)
+
+
+        pe_bundle(i).io_edge_fifo.edge_fifo_ready <> io_fifo.pe_fifo(i).ready
+        pe_bundle(i).io_edge_fifo.edge_fifo_valid <> io_fifo.pe_fifo(i).valid
+        pe_bundle(i).io_edge_fifo.edge_fifo_in <> io_fifo.pe_fifo(i).payload
+
     }
-
-
-
-
-
-
-
 
     io_state.bundle_busy := pe_bundle(1).io_state.pe_busy | pe_bundle(2).io_state.pe_busy | pe_bundle(3).io_state.pe_busy | pe_bundle(4).io_state.pe_busy |
       pe_bundle(5).io_state.pe_busy | pe_bundle(6).io_state.pe_busy | pe_bundle(7).io_state.pe_busy | pe_bundle(8).io_state.pe_busy
-// TO DO:
-// each Bundle is connected to respective fifo
-    pe_bundle(0).io_edge_fifo.edge_fifo_ready <> io_fifo.pe1_fifo.ready
-    pe_bundle(0).io_edge_fifo.edge_fifo_valid <> io_fifo.pe1_fifo.valid
-    pe_bundle(0).io_edge_fifo.edge_fifo_in <> io_fifo.pe1_fifo.payload
 
     val fifo_valid =  Bool()
     fifo_valid :=   io_fifo.pe1_fifo.valid | io_fifo.pe2_fifo.valid | io_fifo.pe3_fifo.valid | io_fifo.pe4_fifo.valid |
