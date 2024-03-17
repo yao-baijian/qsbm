0b92cf6b205792d65545cd62b3845e1a72380c8e
Yao Baijian <byao@connect.ust.hk>
2024-03-17 20:12:31 +0800

    fix redundant logic

--- src/main/scala/PE/DualModeReg.scala
+++ src/main/scala/PE/DualModeReg.scala
@@ -11,14 +11,11 @@ case class DualModeReg(config: PeConfig) extends Component{
     val io = new Bundle{
         val srst        = in Bool()
         val in_stream   = slave Stream(Bits(config.axi_extend_width bits))
-    }
-
-    val io_gather_pe = new Bundle {
-        val wr_valid    = in Bool()
-        val wr_addr     = in UInt (config.addr_width bits)
-        val wr_data     = in Bits (config.data_width bits)
-        val rd_addr     = in UInt (config.addr_width bits)
-        val rd_data     = out Bits (config.data_width bits)
+        val wr_valid = in Bool()
+        val wr_addr = in UInt (config.addr_width bits)
+        val wr_data = in Bits (config.data_width bits)
+        val rd_addr = in UInt (config.addr_width bits)
+        val rd_data = out Bits (config.data_width bits)
     }
 
     //-----------------------------------------------------
@@ -50,8 +47,8 @@ case class DualModeReg(config: PeConfig) extends Component{
         wr_pointer := 0
     }
 
-    when (io_gather_pe.wr_valid) {
-        vertex_reg(io_gather_pe.wr_addr) := io_gather_pe.wr_data
+    when (io.wr_valid) {
+        vertex_reg(io.wr_addr) := io.wr_data
     }
-    io_gather_pe.rd_data := vertex_reg(io_gather_pe.rd_addr)
+    io.rd_data := vertex_reg(io.rd_addr)
 }
